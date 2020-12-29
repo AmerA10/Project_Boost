79aa10bf221041d731506a5692d49b5d7bf9a010
AmerA10 <ajami.amer1@gmail.com>
2020-12-28 17:48:49 -0700

    rocket and some audio

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    AudioSource audio;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource >();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audio.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audio.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+          
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-1 * Vector3.forward);
+          
+        }
+    }
+}
